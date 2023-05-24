5ca0c41655a926c87243934148cd37b61dbbee01
trang112002 <trantrang112002st@gmail.com>
2023-05-24 21:03:32 +0700

    first commit

---
+++ db/migrate/20230524055051_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.integer :phone
+      t.string :address
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
